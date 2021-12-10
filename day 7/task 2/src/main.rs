d141d3472af613a4dd40e1e543e7038ec92da99e
BlackDemonFire <42120952+BlackDemonFire@users.noreply.github.com>
2021-12-10 10:44:01 +0100

    solved day 7

---
+++ day 7/task 2/src/main.rs
@@ -0,0 +1,18 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let contents = read_to_string("input").unwrap();
+    let positions = contents.split(",").map(|pos| pos.parse::<i64>().unwrap());
+    let mut sum = u64::MAX;
+    for cur in positions.clone().min().unwrap()..=positions.clone().max().unwrap() {
+        let mut lsum = u64::MIN;
+        for pos in positions.clone() {
+            lsum += (1..=((pos - cur).abs() as u64)).sum::<u64>();
+        }
+        if lsum < sum {
+            sum = lsum
+        }
+        println!("got {} as sum for aim {}", sum, cur)
+    }
+    println!("{}", sum);
+}
